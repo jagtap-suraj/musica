ed4ec9b393c01ee020640844b33849659259440f
jagtap-suraj <suraj552003@gmail.com>
2025-02-17 00:10:49 +0530

    Add TestimonialCards component and integrate it into the main page; update package.json and tailwind.config.ts for new dependencies and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import NavBar from "@/components/NavBar";
+import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <TestimonialCards />
     </main>
   );
 }
---
+++ src/components/TestimonialCards.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { testimonials } from "../data/testimonials";
+
+import React from "react";
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+
+export default function TestimonialCards() {
+  return (
+    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+      <h2 className="text-3xl font-bold text-center mb-8 z-10">
+        Hear our Harmony: Voices of success
+      </h2>
+      <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-6xl">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
